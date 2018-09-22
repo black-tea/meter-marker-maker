81d651df4d974fd144224b4929ed90628262e2ed
Tim Black <timothy.c.black@gmail.com>
2018-09-21 17:21:09 -0700

    update reactives

--- server.R
+++ server.R
@@ -5,7 +5,6 @@
 library(shiny)
 library(sf) 
 
-
 #### Functions
 ChoosePt <- function(startPt, seg, segFraction) {
   # Ensure interpolation along line returns correct point
@@ -96,8 +95,6 @@ CreateIcon <- function(color) {
   return(custom_icon)
 }
 
-
-
 #### Prep Code
 # Load intersections
 intersections <- read_sf('data/Intersections/Intersections.shp')
@@ -131,37 +128,48 @@ server <- function(input, output, session) {
   })
   
   ### Reactive Objects
-  #RV for location objects
+  # RV for location objects
   rv_location <- reactiveValues(Intersection=list(),
                                 Segment=list())
-  #RV storing UI message variable
+  # RV storing UI message variable
   rv_msg <- reactiveValues()
   
   # RV for location objects
   locationRV <- reactiveValues(Intersection=list(), Segment=list())
   
   # Reactive expression to grab intersection data based on user selection
-  intersection_r <- reactive({
+  intersectionR <- reactive({
     if(!is.null(input$int) && input$int != "" && length(input$int) > 0){
       # int_query <- paste0("SELECT * FROM intersections WHERE tooltip=","'",toString(input$int),"'")
-      # intersection_r <- sqlQuery(int_query, type = 'spatial')
-      intersection_r <- intersections %>%
+      # intersectionR <- sqlQuery(int_query, type = 'spatial')
+      intersectionR <- intersections %>%
         filter(TOOLTIP == toString(input$int))
 
     } else {return(NULL)}
   })
   
+  # Reactive expression to grab cross streets from selected intersection
+  xstreetR <- reactive({
+    if(!is.null(input$int) && input$int != "" && length(input$int) > 0){
+      # Grab selected intersection information
+      intersectionR <- intersectionR()
+      # Query for streets related to the intersection
+      xstreet <- streets %>%
+        filter(INT_ID_FRO == intersectionR$CL_NODE_ID | INT_ID_TO == intersectionR$CL_NODE_ID)
+      return(xstreet)
+      
+    } else {return(NULL)}
+  })
+  
   # Reactive expression to grab the council district based on the point
-  cd_r <- reactive({
+  cdR <- reactive({
     if(!is.null(rv_location$Intersection)){
-      cd_r <- sf::st_join(rv_location$Intersection, councilDistricts)
-      return(cd_r$DISTRICT)
+      cdR <- sf::st_join(rv_location$Intersection, councilDistricts)
+      return(cdR$DISTRICT)
     } else {
       return(NULL)
     }
   })
-  
-  
   
   # Reactive expression to grab the DOT district based on the point
   dotR <- reactive({
@@ -171,19 +179,6 @@ server <- function(input, output, session) {
     } else {
       return(NULL)
     }
-  })
-  
-  # Reactive expression to grab cross streets from selected intersection
-  xstreet_r <- reactive({
-    if(!is.null(input$int) && input$int != "" && length(input$int) > 0){
-      # Grab selected intersection information
-      intersection_r <- intersection_r()
-      # Query for streets related to the intersection
-      xstreet <- streets %>%
-        filter(INT_ID_FRO == intersection_r$CL_NODE_ID | INT_ID_TO == intersection_r$CL_NODE_ID)
-      return(xstreet)
-      
-    } else {return(NULL)}
   })
   
   # # Reactive expression capturing distance for SFS to nearest intersection
@@ -217,74 +212,75 @@ server <- function(input, output, session) {
   observeEvent(input$int, {
     if(!is.null(input$int) && input$int != "" && length(input$int) > 0){
       # Get intersection reactive var, clear markers, clear RV
-      intersection_r <- intersection_r()
+      intersectionR <- intersectionR()
       rv_location$Segment <- NULL
       proxy <- leafletProxy("map") %>%
         clearMarkers() %>%
         clearShapes()
       
       # If there is one marker in the query, it is blue
-      if(nrow(intersection_r) == 1 && length(intersection_r) > 0) {
+      if(nrow(intersectionR) == 1 && length(intersectionR) > 0) {
         # Add intersection to RV object
-        rv_location$Intersection <- intersection_r
+        rv_location$Intersection <- intersectionR
         # Add intersection marker to map
         proxy %>% addAwesomeMarkers(
-          data = intersection_r,
+          data = intersectionR,
           icon = CreateIcon('darkblue')
         )
         
         # if(input$treatment_type == "Intersection Tightening") {
-        #   rv_msg$msg <- c(paste0("Coordinates: ", toString(intersection_r$geometry)),
-        #                   paste0("Council District: ",toString(cd_r())),
+        #   rv_msg$msg <- c(paste0("Coordinates: ", toString(intersectionR$geometry)),
+        #                   paste0("Council District: ",toString(cdR())),
         #                   paste0("DOT District: ", toString(dotR())))
 
 
         # Get cross streets
-        xstreet_r <- xstreet_r()
+        xstreetR <- xstreetR()
 
         # Update message to choose a street
         rv_msg$msg <- c('Select a Cross Street')
 
         # If there is at least one related segment, add it
-        if(nrow(xstreet_r) > 0) {
+        if(nrow(xstreetR) > 0) {
           print("hey you!")
-          print(xstreet_r)
+          print(xstreetR)
           proxy %>% addPolylines(
-            data = xstreet_r,
-            layerId = as.numeric(rownames(xstreet_r)),
+            data = xstreetR,
+            layerId = as.numeric(rownames(xstreetR)),
             color = "gray"
           )
         }
 
         # If there is > 1 marker, gray initially
-      } else if(nrow(intersection_r) > 1) {
+      } else if(nrow(intersectionR) > 1) {
         proxy %>% addAwesomeMarkers(
-          data = intersection_r,
-          layerId = as.numeric(rownames(intersection_r)),
+          data = intersectionR,
+          layerId = as.numeric(rownames(intersectionR)),
           icon = CreateIcon("gray")
         )
         rv_msg$msg <- c('Select One Intersection Node')
         
       }
       # Update the map zoom bounds
-      proxy %>% fitBounds(lng1 = as.double(st_bbox(intersection_r)[1]),
-                          lat1 = as.double(st_bbox(intersection_r)[2]),
-                          lng2 = as.double(st_bbox(intersection_r)[3]),
-                          lat2 = as.double(st_bbox(intersection_r)[4]))
+      proxy %>% fitBounds(lng1 = as.double(st_bbox(intersectionR)[1]),
+                          lat1 = as.double(st_bbox(intersectionR)[2]),
+                          lng2 = as.double(st_bbox(intersectionR)[3]),
+                          lat2 = as.double(st_bbox(intersectionR)[4]))
       
       }
   })
   
   # Map Observer based on int selection
   observeEvent(input$map_marker_click, {
-    if(nrow(intersection_r()) > 1) {
+    if(nrow(intersectionR()) > 1) {
       # Grab ID of the shape that was clicked
       click_id <- input$map_marker_click$id
       # Filter intersections based on the click
-      intS <- intersection_r() %>%
+      intS <- intersectionR() %>%
         filter(rownames(.) == click_id)
       rv_location$Intersection <- intS
-      # Add selected line on top
+      # HERE I NEED TO UPDATE locationRV$point
+      # HERE I NEED TO ADD SELECTED LINE
       proxy <- leafletProxy("map") %>%
         # Add selected intersection to map
         addAwesomeMarkers(
@@ -294,18 +290,18 @@ server <- function(input, output, session) {
         )
       
       rv_msg$msg <- c(paste0("Coordinates: ", toString(rv_location$Intersection$geometry)),
-                      paste0("Council District: ",toString(cd_r())),
+                      paste0("Council District: ",toString(cdR())),
                       paste0("DOT District: ", toString(dotR())))
     }
   })
   
   # Map Observer based on the polyline selection
   observeEvent(input$map_shape_click, {
-    if(!is.null(xstreet_r())){
+    if(!is.null(xstreetR())){
       # Grab ID of the shape that was clicked
       click_id <- input$map_shape_click$id
       # Filter polylines based on the click
-      polyline_s <- xstreet_r() %>%
+      polyline_s <- xstreetR() %>%
         filter(rownames(.) == click_id )
       rv_location$Segment <- polyline_s
       # Add selected line on top as another color
@@ -320,7 +316,6 @@ server <- function(input, output, session) {
       # Once user has selected the street segment, becomes NULL
       rv_msg$msg <- c('.')
     }
-    
   })
   
   # Observer to add point along the line
